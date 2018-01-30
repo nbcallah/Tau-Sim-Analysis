72fa21715b3253ba188f333d2762bdeb91d3e2db
nbcallah <nbcallah@indiana.edu>
2018-01-30 11:49:43 -0500

    Added multilayer quantum reflection using std::complex

--- chisq_spectrum_fit.cpp
+++ chisq_spectrum_fit.cpp
@@ -6,6 +6,7 @@
 #define _USE_MATH_DEFINES
 #include <cmath>
 #include <cstring>
+#include <complex>
 
 extern "C" {
     #include "xorshift.h"
@@ -15,11 +16,22 @@ extern "C" {
 #define GRAV 9.80665e0
 #define JTONEV 6.2415091e27
 #define HBAR 1.054571800e-34
+
 #define NBORON 1.37e29
-#define NCARBON 1.133e29
-#define ACARBON 6.655e-15
 #define ABORON -0.1e-15
 #define SIGMABORON 2200*3.835e-25
+
+#define NCARBON 1.133e29
+#define ACARBON 6.6460e-15
+#define SIGMACARBON 5.551e-28
+
+#define NZINC 2.527e28
+#define AZINC 5.68e-15
+#define SIGMAZINC 5.241e-28
+
+#define NSULFUR 2.527e28
+#define ASULFUR 2.847e-15
+#define SIGMASULFUR 1.556e-28
 
 //std::vector<double> refHist = {24.0,276.0,2147.0,6357.0,6724.0,6445.0,6515.0,6303.0,5883.0,5937.0,5750.0,5862.0,5741.0,5499.0,5298.0,5118.0,5336.0,5208.0,4989.0,4914.0,4840.0,4773.0,4761.0,4603.0,4670.0,4585.0,4490.0,4504.0,4344.0,4234.0,4190.0,4215.0,4159.0,4099.0,4054.0,4042.0,3958.0,3875.0,3758.0,3770.0,6777.0,19591.0,26111.0,24937.0,23352.0,21985.0,21214.0,20137.0,19300.0,18771.0,17531.0,16790.0,16027.0,15480.0,15079.0,14571.0,13997.0,13510.0,13170.0,12590.0,18787.0,32668.0,30531.0,29039.0,27367.0,25942.0,24249.0,22440.0,21718.0,20811.0,19545.0,18862.0,17545.0,16773.0,15964.0,15590.0,14640.0,14114.0,13309.0,12663.0,19992.0,29095.0,27203.0,25432.0,24246.0,22596.0,21316.0,20074.0,18979.0,17967.0,16720.0,16213.0,15231.0,14874.0,13783.0,13126.0,12620.0,12152.0,11748.0,11099.0,19469.0,31086.0,29762.0,28316.0,26476.0,24893.0,23408.0,22080.0,21091.0,19686.0,18667.0,17434.0,16717.0,15647.0,15110.0,14155.0,13375.0,12639.0,12072.0,11831.0,18263.0,23110.0,22100.0,21373.0,20302.0,19436.0,18349.0,17475.0,16732.0,15687.0,14945.0,14201.0,13216.0,12934.0,12358.0,11613.0,11052.0,10341.0,9803.0,9612.0,15876.0,20286.0,19403.0,17943.0,16981.0,15999.0,14919.0,13821.0,12959.0,11977.0,11107.0,10590.0,9923.0,9119.0,8469.0,8032.0,7199.0,6634.0,6475.0,5908.0,8792.0,10150.0,8870.0,7618.0,6546.0,5978.0,5135.0,4541.0,3926.0,3486.0,3176.0,2779.0,2399.0,2159.0,1892.0,1670.0,1493.0,1387.0,1236.0,1038.0,959.0,787.0,739.0,663.0,589.0};
 std::vector<double> refHist = {276.0,2147.0,6357.0,6724.0,6445.0,6515.0,6303.0,5883.0,5937.0,5750.0,5862.0,5741.0,5499.0,5298.0,5118.0,5336.0,5208.0,4989.0,4914.0,4840.0,4773.0,4761.0,4603.0,4670.0,4585.0,4490.0,4504.0,4344.0,4234.0,4190.0,4215.0,4159.0,4099.0,4054.0,4042.0,3958.0,3875.0,3758.0,3770.0,6777.0,19591.0,26111.0,24937.0,23352.0,21985.0,21214.0,20137.0,19300.0,18771.0,17531.0,16790.0,16027.0,15480.0,15079.0,14571.0,13997.0,13510.0,13170.0,12590.0,18787.0,32668.0,30531.0,29039.0,27367.0,25942.0,24249.0,22440.0,21718.0,20811.0,19545.0,18862.0,17545.0,16773.0,15964.0,15590.0,14640.0,14114.0,13309.0,12663.0,19992.0,29095.0,27203.0,25432.0,24246.0,22596.0,21316.0,20074.0,18979.0,17967.0,16720.0,16213.0,15231.0,14874.0,13783.0,13126.0,12620.0,12152.0,11748.0,11099.0,19469.0,31086.0,29762.0,28316.0,26476.0,24893.0,23408.0,22080.0,21091.0,19686.0,18667.0,17434.0,16717.0,15647.0,15110.0,14155.0,13375.0,12639.0,12072.0,11831.0,18263.0,23110.0,22100.0,21373.0,20302.0,19436.0,18349.0,17475.0,16732.0,15687.0,14945.0,14201.0,13216.0,12934.0,12358.0,11613.0,11052.0,10341.0,9803.0,9612.0,15876.0,20286.0,19403.0,17943.0,16981.0,15999.0,14919.0,13821.0,12959.0,11977.0,11107.0,10590.0,9923.0,9119.0,8469.0,8032.0,7199.0,6634.0,6475.0,5908.0,8792.0,10150.0,8870.0,7618.0,6546.0,5978.0,5135.0,4541.0,3926.0,3486.0,3176.0,2779.0,2399.0,2159.0,1892.0,1670.0,1493.0,1387.0,1236.0,1038.0,959.0,787.0,739.0,663.0,589.0};
@@ -36,10 +48,10 @@ typedef struct weightedBin {
     double num;
 } weightedBin;
 
-bool absorb(double ePerp, double u, double contam) {
+bool absorb(double ePerp, double u, double fracCarb) {
 //    printf("%f\n", contam);
-    double v = 2*M_PI*HBAR*HBAR*(NCARBON*contam)*ACARBON/MASS_N + 2*M_PI*HBAR*HBAR*NBORON*ABORON/MASS_N;
-    double w = HBAR*NBORON*SIGMABORON/2;
+    double v = 2*M_PI*HBAR*HBAR*(NCARBON*fracCarb)*ACARBON/MASS_N + 2*M_PI*HBAR*HBAR*(NBORON*(1-fracCarb))*ABORON/MASS_N;
+    double w = HBAR*(NBORON*(1-fracCarb))*SIGMABORON/2;
     double alpha = sqrt((v-ePerp)*(v-ePerp) + w*w);
     double prob = 1 - 
         (ePerp - sqrt(ePerp)*sqrt(2*alpha - 2*(v-ePerp)) + alpha)
@@ -49,7 +61,62 @@ bool absorb(double ePerp, double u, double contam) {
     return u < prob ? true : false;
 }
 
-std::vector<weightedBin> createHistQuant(double contam, double threshold, double saturation, std::vector<evt>& events, double* randU01s, double* randDeathTimes) {
+std::vector<std::complex<double>> matmul(std::vector<std::complex<double>> a, std::vector<std::complex<double>> b) {
+    std::vector<std::complex<double>> res = {std::complex<double>(0,0), std::complex<double>(0,0), std::complex<double>(0,0), std::complex<double>(0,0)};
+    if(a.size() != 4 || b.size() != 4) {
+        return res;
+    }
+    res[0] = a[0]*b[0] + a[1]*b[2];
+    res[1] = a[0]*b[1] + a[1]*b[3];
+    res[2] = a[2]*b[0] + a[3]*b[2];
+    res[3] = a[2]*b[1] + a[3]*b[3];
+    return res;
+}
+
+std::complex<double> k(double ePerp, std::complex<double> u) {
+    return std::sqrt((2*MASS_N/(HBAR*HBAR))*(ePerp - u));
+}
+
+std::complex<double> gamma(std::complex<double> kn, std::complex<double> knm1) {
+    return knm1/kn;
+}
+
+std::vector<std::complex<double>> m(std::complex<double> kn, std::complex<double> knm1, double z) {
+    std::vector<std::complex<double>> res = {std::complex<double>(0,0), std::complex<double>(0,0), std::complex<double>(0,0), std::complex<double>(0,0)};
+    res[0] = (1.0/2.0)*(1.0 + gamma(kn,knm1))*std::exp(std::complex<double>(0,1)*(knm1-kn)*z);
+    res[1] = (1.0/2.0)*(1.0 - gamma(kn,knm1))*std::exp(-std::complex<double>(0,1)*(knm1+kn)*z);
+    res[2] = (1.0/2.0)*(1.0 - gamma(kn,knm1))*std::exp(std::complex<double>(0,1)*(knm1+kn)*z);
+    res[3] = (1.0/2.0)*(1.0 + gamma(kn,knm1))*std::exp(-std::complex<double>(0,1)*(knm1-kn)*z);
+    return res;
+}
+
+bool absorbMultilayer(double ePerp, double u, double thick) {
+    const double vcarbon = (2*M_PI*(HBAR*HBAR)/MASS_N)*ACARBON*NCARBON;
+    const double wcarbon = (HBAR/2)*NCARBON*SIGMACARBON;
+    const double vboron = (2*M_PI*(HBAR*HBAR)/MASS_N)*ABORON*NBORON;
+    const double wboron = (HBAR/2)*NBORON*SIGMABORON;
+    const double vzns = (2*M_PI*(HBAR*HBAR)/MASS_N)*AZINC*NZINC + (2*M_PI*(HBAR*HBAR)/MASS_N)*ASULFUR*NSULFUR;
+    const double wzns = (HBAR/2)*NZINC*SIGMAZINC + (HBAR/2)*NSULFUR*SIGMASULFUR;
+    
+    std::vector<std::complex<double>> pots = {std::complex<double>(0, 0),
+                                              std::complex<double>(vcarbon, -wcarbon),
+                                              std::complex<double>(vboron, -wboron),
+                                              std::complex<double>(vzns, -wzns)};
+    std::vector<std::complex<double>> mbar = {std::complex<double>(1,0), std::complex<double>(0,0), std::complex<double>(0,0), std::complex<double>(1,0)};
+    std::vector<double> zs = {0.0, thick*1e-9, thick*1e-9 + 20e-9, 10000e-9};
+    
+    for(int i = pots.size(); i > 0; i--) {
+        mbar = matmul(mbar, m(k(ePerp, pots[i]), k(ePerp, pots[i-1]), zs[i-1]));
+    }
+    
+    double refl = (std::conj(-mbar[2]/mbar[3])*-mbar[2]/mbar[3]).real();    
+    if(u < (1-refl)) {
+        return true;
+    }
+    return false; 
+}
+
+std::vector<weightedBin> createHistQuant(double fracCarb, double threshold, double saturation, std::vector<evt>& events, double* randU01s, double* randDeathTimes) {
 //void createHist(double absProb, double absCut, double threshold, double saturation, std::vector<evt>& events, double* randU01s) {
     std::vector<weightedBin> hist;
     weightedBin zero = {0.0, 0.0};
@@ -76,7 +143,79 @@ std::vector<weightedBin> createHistQuant(double contam, double threshold, double
             if(events[i].times[j] >= 225) {
                 break;
             }
-            if(absorb(events[i].ePerp[j], randU01s[i*100 + j], contam)) {
+            if(absorb(events[i].ePerp[j], randU01s[i*100 + j], fracCarb)) {
+                if(int(events[i].times[j])-41 > 183) {
+                    printf("Boo!\n");
+                }
+                hist[int(events[i].times[j])-41].wgt += weight;
+                hist[int(events[i].times[j])-41].wgtSqr += weight*weight;
+                hist[int(events[i].times[j])-41].num += 1;
+                break;
+            }
+        }
+    }
+    return hist;
+}
+
+std::vector<weightedBin> createHistQuantEdE(double fracCarb, double threshold, std::vector<evt>& events, double* randU01s, double* randDeathTimes) {
+//void createHist(double absProb, double absCut, double threshold, double saturation, std::vector<evt>& events, double* randU01s) {
+    std::vector<weightedBin> hist;
+    weightedBin zero = {0.0, 0.0};
+    hist.resize(184, zero);
+    for(unsigned long i = 0; i < events.size(); i++) {
+        double weight = 0.0;
+        if(events[i].energy*JTONEV < threshold) {
+            continue;
+        }
+        weight = events[i].energy*JTONEV/34.5;
+
+        for(int j = 0; j < 50; j++) {
+            if(events[i].times[j] < 41) {
+                continue;
+            }
+            if(events[i].times[j] - 41 > randDeathTimes[i]) {
+                break;
+            }
+            if(events[i].times[j] >= 225) {
+                break;
+            }
+            if(absorb(events[i].ePerp[j], randU01s[i*100 + j], fracCarb)) {
+                if(int(events[i].times[j])-41 > 183) {
+                    printf("Boo!\n");
+                }
+                hist[int(events[i].times[j])-41].wgt += weight;
+                hist[int(events[i].times[j])-41].wgtSqr += weight*weight;
+                hist[int(events[i].times[j])-41].num += 1;
+                break;
+            }
+        }
+    }
+    return hist;
+}
+
+std::vector<weightedBin> createHistQuantMultilayerEdE(double thick, double threshold, std::vector<evt>& events, double* randU01s, double* randDeathTimes) {
+//void createHist(double absProb, double absCut, double threshold, double saturation, std::vector<evt>& events, double* randU01s) {
+    std::vector<weightedBin> hist;
+    weightedBin zero = {0.0, 0.0};
+    hist.resize(184, zero);
+    for(unsigned long i = 0; i < events.size(); i++) {
+        double weight = 0.0;
+        if(events[i].energy*JTONEV < threshold) {
+            continue;
+        }
+        weight = events[i].energy*JTONEV/34.5;
+
+        for(int j = 0; j < 50; j++) {
+            if(events[i].times[j] < 41) {
+                continue;
+            }
+            if(events[i].times[j] - 41 > randDeathTimes[i]) {
+                break;
+            }
+            if(events[i].times[j] >= 225) {
+                break;
+            }
+            if(absorbMultilayer(events[i].ePerp[j], randU01s[i*100 + j], thick)) {
                 if(int(events[i].times[j])-41 > 183) {
                     printf("Boo!\n");
                 }
@@ -335,9 +474,9 @@ int main(int argc, char** argv) {
     for(int i = 0; i < nBins+1; i++) {
         for(int j = 0; j < nBins+1; j++) {
             for(int k = 0; k < nBins+1; k++) {
-                double thresh = 2.0+4.0*i/(double)nBins;
-                double sat = 24+10.5*j/(double)nBins;
-                double contam = 1.0/(2+5*k/(double)nBins);
+                double thresh = 2.0 + 4.0*i/(double)nBins;
+                double sat = 24 + 10.5*j/(double)nBins;
+                double contam = 0.31 + 0.04*k/(double)nBins;
                 std::vector<weightedBin> hist1 = createHistQuant(contam, thresh, sat, events, randU01s, randDeathTimes);
                 double chisqWgt = calcChisqWgt(refHist, hist1);
                 double chisqUnWgt = calcChisq(refHist, hist1);
@@ -348,6 +487,50 @@ int main(int argc, char** argv) {
             }
         }
     }*/
+    
+    /*int nBins = 20;
+    #pragma omp parallel for collapse(2)
+    for(int i = 0; i < nBins+1; i++) {
+        for(int j = 0; j < nBins+1; j++) {
+            double thresh = 2.0 + 4.0*i/(double)nBins;
+            double contam = 0.31 + 0.04*j/(double)nBins;
+            std::vector<weightedBin> hist1 = createHistQuantEdE(contam, thresh, events, randU01s, randDeathTimes);
+            double chisqWgt = calcChisqWgt(refHist, hist1);
+            double chisqUnWgt = calcChisq(refHist, hist1);
+            double chisqNate = calcChisqNate(refHist, hist1);
+//                printf("%f %f %f %f\n", GRAV*MASS_N*(0.01+0.05*i/10.0)*JTONEV, GRAV*MASS_N*(0.1+0.02*j/10.0)*JTONEV, 0.5+0.5*k/10.0, chisq/hist1.size());
+            printf("%f %f %f %f %f\n", contam, thresh, chisqWgt/hist1.size(), chisqUnWgt/hist1.size(), chisqNate/hist1.size());
+            fflush(stdout);
+        }
+    }*/
+    
+    int nBins = 20;
+    #pragma omp parallel for collapse(2)
+    for(int i = 0; i < nBins+1; i++) {
+        for(int j = 0; j < nBins+1; j++) {
+            double thresh = 2.0 + 4.0*i/(double)nBins;
+            double thick = 0 + 20*j/(double)nBins;
+            std::vector<weightedBin> hist1 = createHistQuantMultilayerEdE(thick, thresh, events, randU01s, randDeathTimes);
+            double chisqWgt = calcChisqWgt(refHist, hist1);
+            double chisqUnWgt = calcChisq(refHist, hist1);
+            double chisqNate = calcChisqNate(refHist, hist1);
+//                printf("%f %f %f %f\n", GRAV*MASS_N*(0.01+0.05*i/10.0)*JTONEV, GRAV*MASS_N*(0.1+0.02*j/10.0)*JTONEV, 0.5+0.5*k/10.0, chisq/hist1.size());
+            printf("%f %f %f %f %f\n", thick, thresh, chisqWgt/hist1.size(), chisqUnWgt/hist1.size(), chisqNate/hist1.size());
+            fflush(stdout);
+        }
+    }
+    
+    /*int nBins = 100;
+    for(int i = 0; i < nBins+1; i++) {
+        double contam = 0.2 + 0.2*i/(double)nBins;
+        std::vector<weightedBin> hist1 = createHistQuantEdE(contam, 0.0, events, randU01s, randDeathTimes);
+        double chisqWgt = calcChisqWgt(refHist, hist1);
+        double chisqUnWgt = calcChisq(refHist, hist1);
+        double chisqNate = calcChisqNate(refHist, hist1);
+//                printf("%f %f %f %f\n", GRAV*MASS_N*(0.01+0.05*i/10.0)*JTONEV, GRAV*MASS_N*(0.1+0.02*j/10.0)*JTONEV, 0.5+0.5*k/10.0, chisq/hist1.size());
+        printf("%f %f %f %f %f\n", contam, 0.0, chisqWgt/hist1.size(), chisqUnWgt/hist1.size(), chisqNate/hist1.size());
+        fflush(stdout);
+    }*/
 
 //    std::vector<weightedBin> hist1 = createHist(0.155, 0.0, 15.377918, 17.428307, events, randU01s, randDeathTimes);
 //    std::vector<weightedBin> hist1 = createHist(0.26, 2.1, 5.125973, 30.755837, events, randU01s, randDeathTimes);
@@ -357,14 +540,19 @@ int main(int argc, char** argv) {
 //      std::vector<weightedBin> hist1 = createHist(0.285, 2.6, 4.100778, 34.5, events, randU01s, randDeathTimes); //Prof II
 //      std::vector<weightedBin> hist1 = createHistQuant(0.1, 4.100778, 34.5, events, randU01s, randDeathTimes); //Test of quantum
 //      std::vector<weightedBin> hist1 = createHistQuant(0.4, 4.100778, 34.5, events, randU01s, randDeathTimes); //Test of quantum
-      std::vector<weightedBin> hist1 = createHistQuant(0.444444, 3.8, 34.5, events, randU01s, randDeathTimes); //Test of quantum
-      double chisq = calcChisqWgt(refHist, hist1);
+//      std::vector<weightedBin> hist1 = createHistQuant(0.444444, 3.8, 34.5, events, randU01s, randDeathTimes); //Test of quantum
+//      std::vector<weightedBin> hist1 = createHistQuant(0.363636, 5.8, 34.5, events, randU01s, randDeathTimes); //Test of quantum frac
+      //std::vector<weightedBin> hist1 = createHistQuant(0.35, 4.4, 34.5, events, randU01s, randDeathTimes); //quant frac zoom
+//      std::vector<weightedBin> hist1 = createHistQuant(0.31, 6.0, 24.0, events, randU01s, randDeathTimes); //quant frac zoom
+//      std::vector<weightedBin> hist1 = createHistQuantEdE(0.35, 0.0, events, randU01s, randDeathTimes); //quant frac zoom
+//      std::vector<weightedBin> hist1 = createHistQuantEdE(0.32, 0.0, events, randU01s, randDeathTimes); //quant EdE opt, no cutoff
+/*      double chisq = calcChisqWgt(refHist, hist1);
       double chisq2 = calcChisq(refHist, hist1);
       printf("%f %f\n\n", chisq/hist1.size(), chisq2/hist1.size());
       for(auto it = hist1.begin(); it < hist1.end(); it++) {
           printf("%f,", it->wgt);
       }
-      printf("\n");
+      printf("\n");*/
 
 /*    char paramFile[256];
     char histFile[256];
